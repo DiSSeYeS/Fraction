b5ee34c20f997d6ba8264461f4aac513c71c54c3
DiSSeYeS <mozglifovich@gmail.com>
2025-06-23 17:26:23 +0300

    Добавьте файлы проекта.

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0 && numerator != 0)
+            {
+                throw new Exception("The denominator can not be zero.");
+            }
+
+            if (numerator == 0)
+            {
+                this.numerator = 0;
+                this.denominator = 0;
+                return;
+            }
+
+            for (int i = denominator; i >= 2; i--)
+            {
+                if (denominator % i == 0 && numerator % i == 0)
+                {
+                    denominator /= i;
+                    numerator /= i;
+                }
+            }
+
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Fraction(Fraction numerator, Fraction denominator) : this((numerator / denominator).numerator, (numerator / denominator).denominator)
+        {
+        }
+        public Fraction(Fraction numerator, int denominator) : this((numerator / denominator).numerator, (numerator / denominator).denominator)
+        {
+        }
+        public Fraction(int numerator, Fraction denominator) : this((numerator / denominator).numerator, (numerator / denominator).denominator)
+        {
+        }
+        public Fraction(double num) : this(ConvertDoubleToFraction(num).Item1, ConvertDoubleToFraction(num).Item2)
+        {  
+        }
+        public Fraction(decimal num) : this(ConvertDecimalToFraction(num).Item1, ConvertDecimalToFraction(num).Item2)
+        {
+        }
+
+        private static Tuple<int, int> ConvertDoubleToFraction(double num)
+        {
+            int numberBeforePoint = int.Parse(num.ToString().Split(",").First());
+            int numberAfterPoint = int.Parse(num.ToString().Split(",").Last());
+            int countOfDigitsAfterPoint = num.ToString().Split(",").Last().Length;
+            int denominator = (int)Math.Pow(10, countOfDigitsAfterPoint);
+
+            return Tuple.Create(numberBeforePoint * denominator + numberAfterPoint, denominator);
+        }
+        private static Tuple<int, int> ConvertDecimalToFraction(decimal num)
+        {
+            int numberBeforePoint = int.Parse(num.ToString().Split(",").First());
+            int numberAfterPoint = int.Parse(num.ToString().Split(",").Last());
+            int countOfDigitsAfterPoint = num.ToString().Split(",").Last().Length;
+            int denominator = (int)Math.Pow(10, countOfDigitsAfterPoint);
+
+            return Tuple.Create(numberBeforePoint * denominator + numberAfterPoint, denominator);
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            if (a.denominator == b.denominator)
+            {
+                return new Fraction(a.numerator + b.numerator, a.denominator);
+            }
+
+            return new Fraction(a.numerator * b.denominator + b.numerator * a.denominator, a.denominator * b.denominator);
+        }
+        public static Fraction operator +(Fraction a, int b) => new Fraction(a.numerator + a.denominator * b, a.denominator);
+        public static Fraction operator +(int a, Fraction b) => b + a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.numerator, a.denominator);
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+        public static Fraction operator -(Fraction a, int b) => a + (-b);
+        public static Fraction operator -(int a, Fraction b) => (-b) + a;
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.numerator * b.numerator, a.denominator * b.denominator);
+        public static Fraction operator *(Fraction a, int b) => new Fraction(a.numerator * b, a.denominator);
+        public static Fraction operator *(int a, Fraction b) => b * a;
+        public static Fraction operator /(Fraction a, Fraction b) => new Fraction(a.numerator * b.denominator, a.denominator * b.numerator);
+        public static Fraction operator /(Fraction a, int b) => a * new Fraction(1, b);
+        public static Fraction operator /(int a, Fraction b) => new Fraction(a * b.numerator, b.denominator);
+        public static Fraction Pow(Fraction fraction, int power)
+        {
+            Fraction oldFraction = new Fraction(fraction.numerator, fraction.denominator);
+
+            for (int i = 0; i < power - 1; i++)
+            {
+                fraction *= oldFraction;
+            }
+
+            return new Fraction(fraction.numerator, fraction.denominator);
+        }
+
+        public override string ToString()
+        {
+            int integers = 0;
+
+            while (numerator >= denominator)
+            {
+                numerator -= denominator;
+                integers++;
+            }
+
+            if (integers == 0)
+            {
+                return $"{numerator}/{denominator}";
+            }
+            if (numerator == 0)
+            {
+                return integers.ToString();
+            }
+            return $"{integers}({numerator}/{denominator})";
+        }
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Fraction
+{
+    public class Program()
+    {
+        public static void Main()
+        {
+            Fraction fraction1 = new Fraction(4, 5);
+            Fraction fraction2 = new Fraction(2, 11);
+
+            Console.WriteLine($"{fraction1} x {fraction2} = {fraction1 * fraction2}");
+
+            Console.WriteLine(Fraction.Pow(fraction1, 5));
+
+            int a = 4;
+
+            Fraction fraction3 = new Fraction(
+                (int)(15 * Math.Pow(Math.Pow(a, 1.0 / 28.0),1.0 / 5.0) - 7 * Math.Pow(Math.Pow(a, 1.0 / 20.0), 1.0 / 7.0)),
+                (int)(2 * Math.Pow(Math.Pow(a, 1.0 / 4.0), 1.0 / 35.0)));
+
+            Console.WriteLine(fraction3);
+
+            Fraction fraction4 = new Fraction(fraction1, fraction2);
+
+            Console.WriteLine(fraction4);
+
+            Console.WriteLine(4 + fraction4);
+        }
+    }
+}
